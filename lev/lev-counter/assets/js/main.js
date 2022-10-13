9fc7b5c269fc2dfffb27d84702795a3e41b5b7ea
NamlidN <dilman.n999@gmail.com>
2022-10-13 14:41:15 +0200

    rechner

---
+++ lev/lev-counter/assets/js/main.js
@@ -0,0 +1,55 @@
+let a=0
+document.getElementById('e').innerHTML=a
+function asdf(){
+
+let = a++
+console.log(a)
+document.getElementById('e').innerHTML = a
+}
+function ab(){
+
+  a--
+    console.log(a)
+    document.getElementById('e').innerHTML = a
+    }
+
+    function abc(){
+
+a= a+10
+        console.log(a)
+        document.getElementById('e').innerHTML = a
+        }
+        
+        
+            function abcd(){
+
+            a= a-10
+            console.log(a)
+ document.getElementById('e').innerHTML = a
+    }
+function abcde(){
+
+                a= a+100
+console.log(a)
+ document.getElementById('e').innerHTML = a
+ }
+
+ function abcdef(){
+
+a= a-100
+console.log(a)
+document.getElementById('e').innerHTML = a
+}
+function re(){
+a= 0
+ console.log(a)
+document.getElementById('e').innerHTML = a
+ }
+function mal(){
+
+                            a= a*2
+console.log(a)
+document.getElementById('e').innerHTML = a
+}
+
+
